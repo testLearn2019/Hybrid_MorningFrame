aad877f98e1bde08dd4764198389f642c5ce2d1f
c <c@192.168.1.4>
2020-08-18 09:25:29 +0530

    added comments in GlobalReusables

--- FrameworkDemo/src/test/java/com/utilities/GlobalReusables.java
+++ FrameworkDemo/src/test/java/com/utilities/GlobalReusables.java
@@ -10,7 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+// This class reads external files
 public class GlobalReusables {
 	
 	String keyValue;
